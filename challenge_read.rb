802577e6c464d83b9c357471d05a7d950f5c1e50
dpatel222 <deeppatel2@rrc.ca>
2024-05-25 19:42:29 +0000

    Complted the forst 2 parts of quesiton 1

--- challenge_read.rb
+++ challenge_read.rb
@@ -2,4 +2,31 @@ require_relative 'ar.rb'
 
 number_of_products = Product.count
 
-puts "There are #{number_of_products} in the products table."
+first_product = Product.first[:name]
+
+# puts first_product
+
+all_producs = Product.all
+
+# the inspect tells us that there are 8 coloums which are
+# product_id
+# name
+# descrpition
+# price
+# stock_quantity
+# category_id
+# created_at
+# updated_at
+
+# puts all_producs[0].inspect
+
+price_name = Product.where('price > 10 AND name LIKE "C%"')
+ 
+price_name.each{ |p| puts p.name}
+price_name.each{ |p| puts p.price}
+
+
+
+# We see that there is a category_id coloum so we can assume it has a assocaition with the category table
+
+# puts "There are #{number_of_products} in the products table."
