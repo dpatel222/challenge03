4ed53a733089dc69d16b3c612af9ccbc66c3cd8f
dpatel222 <deeppatel2@rrc.ca>
2024-05-26 15:08:01 -0500

    Finished the Update challenge

--- challenge_update.rb
+++ challenge_update.rb
@@ -1,8 +1,13 @@
 require_relative 'ar.rb'
 
+before = Product.where("stock_quantity >= 40")
+before.each{|c| puts c.stock_quantity}
+
 stock = Product.where("stock_quantity > 40")
 
-stck.each do |c|
-    c.stock_quantity + 1
+stock.each do |c|
+    c.stock_quantity = c.stock_quantity + 1
     c.save
+    
 end
+stock.each{|c| puts c.stock_quantity}
