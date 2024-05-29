84de6f9f2fccea02f1fd41d2109fc7503d923c27
dpatel222 <deeppatel2@rrc.ca>
2024-05-28 19:19:34 -0500

    Finished faker

--- challenge_faker.rb
+++ challenge_faker.rb
@@ -1,21 +1,42 @@
 require_relative 'ar.rb'
 require 'faker'
 
-all_cat = Category.all
+all_cat = Category.all.includes(:products)
 
-for i in 0..19
-    puts all_cat[i].inspect
+for i in 0..20
+  puts all_cat[i].inspect
 end
 
-for i in 1..10
-    new_cat = Category.create(name: Faker::Book.genre)
-    for x in 1..10
-        new_cat.products.build( name: Faker::Book.title,
-                          description: Faker::Book.author,
-                          price: Faker::Number.number(digits: 2),
-                          stock_quantity: Faker::Number.number(digits: 2)
+=begin 10.times do
+    new_cat = Category.create(name: Faker::Pokemon.name)
 
-        )
+    10.times do
+      new_cat.products.create(
+        name: Faker::Book.title,
+        description: Faker::Book.author,
+        price: Faker::Number.number(2),
+        stock_quantity: Faker::Number.number(2)
+      )
     end
 
 end
+=end
+
+all_products = Product.all
+
+=begin for i in 0..100
+    puts all_products[i].inspect
+end
+=end
+
+
+
+
+all_cat.each do |category|
+    puts "Category: #{category.name}"
+    category.products.each do |product|
+      puts "  Product: #{product.name}, Price: $#{product.price}"
+    end
+  
+end 
+
